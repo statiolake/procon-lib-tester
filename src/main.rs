77efaf553b3f2025dba3125f236c44d33f2ce83d
statiolake <statiolake@gmail.com>
2018-11-25 15:54:26 +0900

    remove unnecessary box_err function

--- src/main.rs
+++ src/main.rs
@@ -100,7 +100,7 @@ fn check_root(path: &Path) -> bool {
 
 /// ライブラリのルートディレクトリを検索します。
 fn find_lib_root() -> Result<PathBuf> {
-    let mut curr = env::current_dir().map_err(error_into_box)?;
+    let mut curr = env::current_dir()?;
     // これはダミーのファイル名。
     // parent() で、カレントディレクトリから取得できるようにするため...
     curr.push("dummy");
@@ -133,22 +133,4 @@ fn enumerate_tests(target: &Path) -> io::Result<Vec<Test>> {
     }
 
     Ok(result)
-}
-
-/// `E: Error` を `Box<Error>` へ突っ込む関数です。通常こういう `box` 化は
-/// `Result::map_error()` に渡すクロージャの形で `|err| Box::new(err)` とするこ
-/// とが多いと思いますが、これ結構失敗します。というのも通常のケースでは `err`
-/// は具体的な型の値なので、 `Box<SomeConcreteType>` の形に推論されてしまうから
-/// です。その後たとえば `?` 演算子で早期リターンしようにも、
-/// `Box<SomeConcreteType>` から`Box<dyn Error>` への型強制及び `From` 実装はな
-/// いので、だいたいエラーになってしまいます。これを回避するには `Box::new(err)
-/// as Box<dyn Error>` などとしなければならず、タイプ数が増えます。
-///
-/// そこでこの関数を噛ませることができるのです。これを使えばそもそも明示的に
-/// `Box<dyn Error>` を返すようになっているので意図しない型に推論されることはあ
-/// りません！やったね。
-fn error_into_box<E: 'static + std::error::Error + Send + Sync>(
-    error: E,
-) -> Box<std::error::Error + Send + Sync> {
-    Box::new(error)
 }
